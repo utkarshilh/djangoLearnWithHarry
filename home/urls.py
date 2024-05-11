9ac56dfa0a347381884827a7f69432dd2daab638
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-11 18:58:53 +0530

    a

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,9 @@ from home import views
 
 urlpatterns = [
     path("", views.index, name='home'),
-    path("about", views.about, name='about'),
+    path("aboutus", views.aboutus, name='aboutus'),
     path("services", views.services, name='services'),
-    path("contacts", views.contacts, name='contacts')
+    path("contactus", views.contactus, name='contactsus')
 
 
 ]
